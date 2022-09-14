cf0fde5c6eb4dae10f73d01a1343953276760a5b
Joshua Reynolds <joshua.reynolds@bettermarks.com>
2022-09-14 14:00:48 +0200

    fix: just having one failure when product not found

--- consumer/src/api.pact.spec.js
+++ consumer/src/api.pact.spec.js
@@ -51,34 +51,6 @@ describe("API Pact test", () => {
         ]);
       });
     });
-
-    test("no products exists", async () => {
-      // set up Pact interactions
-      await provider.addInteraction({
-        states: [{ description: "no products exist" }],
-        uponReceiving: "get all products",
-        withRequest: {
-          method: "GET",
-          path: "/products",
-        },
-        willRespondWith: {
-          status: 200,
-          headers: {
-            "Content-Type": "application/json; charset=utf-8",
-          },
-          body: [],
-        },
-      });
-
-      await provider.executeTest(async (mockService) => {
-        const api = new API(mockService.url);
-
-        // make request to Pact mock server
-        const product = await api.getAllProducts();
-
-        expect(product).toStrictEqual([]);
-      });
-    });
   });
 
   describe("getting one product", () => {
